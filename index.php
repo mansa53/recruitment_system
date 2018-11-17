8e3d56227753d241f8902cc2dd6f1d8d06c885a4
mansa53 <31716134+mansa53@users.noreply.github.com>
2018-11-17 13:04:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE HTML>
+<!--
+	Projection by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Hire</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="index.php" class="logo"><strong>Welcome to Hire</strong></a>
+					<nav id="nav">
+						<a href="index.php">Home</a>
+						<a href="company/companyloginpage.php">Company LogIn</a>
+						<a href="candidate/loginpage.php">Aspirant LogIn</a>
+					</nav>
+					<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+				</div>
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<header>
+						<h1>Welcome to Hire</h1>
+					</header>
+
+					<div class="flex ">
+
+						<div>
+							<span class="icon fa-car"></span>
+							<h3>Search jobs</h3>
+							<p>Choose your ideal job</p>
+						</div>
+
+						<div>
+							<span class="icon fa-camera"></span>
+							<h3>Search  talent</h3>
+							<p>Choose the best talent</p>
+						</div>
+
+						
+					</div>
+
+					<footer>
+						<a href="index.php" class="button">Get Started</a>
+					</footer>
+				</div>
+			</section>
+
+
+		<!-- Three -->
+			<section id="three" class="wrapper align-center">
+				<div class="inner">
+					<div class="flex flex-2">
+						<article>
+							<div class="image round">
+								<img src="images/candidateimage.jpg" alt="Pic 01" />
+							</div>
+							<header>
+								<h3>You are a aspirant?</h3>
+							</header>
+							<p>Apply for the best jobs available!</p>
+							<footer>
+								<a href="candidate/create-accnt.php" class="button">Join</a>
+							</footer>
+						</article>
+						<article>
+							<div class="image round">
+								<img src="images/compimage.jpg" alt="Pic 02" />
+							</div>
+							<header>
+								<h3>You are a company?</h3>
+							</header>
+							<p>Choose from the best set of talents out there</p>
+							<footer>
+								<a href="company/createc-account.php" class="button">Join</a>
+							</footer>
+						</article>
+					</div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+
+					<h3>Get in touch</h3>
+
+					<form action="#" method="post">
+
+						<div class="field half first">
+							<label for="name">Name</label>
+							<input name="name" id="name" type="text" placeholder="Name">
+						</div>
+						<div class="field half">
+							<label for="email">Email</label>
+							<input name="email" id="email" type="email" placeholder="Email">
+						</div>
+						<div class="field">
+							<label for="message">Message</label>
+							<textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
+						</div>
+						<ul class="actions">
+							<li><input value="Send Message" class="button alt" type="submit"></li>
+						</ul>
+					</form>
+
+					
+
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
